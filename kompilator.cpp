d3dc0a15c6e4a345b25ee41d0dcdec1e192ed84e
Hjalmar Öhman <87278958+Hjalmar-Ohman@users.noreply.github.com>
2022-11-08 21:58:42 +0800

    Initial Upload

---
+++ kompilator.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    unordered_map<string, stack<string>> variables; // map of stack where key is the variable and the value is the types it has been declared as
+    stack<unordered_map<string, string>> scope; // keeps track of this scope
+    unordered_map<string, string> new_map;
+    scope.push(new_map);
+
+    string command;
+    string variable;
+    string type;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> command;
+        if (command == "{") {
+            unordered_map<string, string> new_map;
+            scope.push(new_map);
+        } else if (command == "}") {
+
+            for (auto& it : scope.top()) {
+                variables[it.first].pop();
+            }
+            scope.pop();
+        } else if (command == "TYPEOF") {
+            cin >> variable;
+            if (variables[variable].empty()) {
+                cout << "UNDECLARED"
+                     << "\n";
+            } else {
+                cout << variables[variable].top() << "\n";
+            }
+        } else if (command == "DECLARE") {
+            cin >> variable;
+            cin >> type;
+            if (scope.top()[variable].empty()) {
+                variables[variable].push(type);
+                scope.top()[variable] = type;
+            } else {
+                cout << "MULTIPLE DECLARATION"
+                     << "\n";
+                break;
+            }
+        }
+    }
+}
